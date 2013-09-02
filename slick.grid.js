154b7077bbd5f606adeb32a790ab7440fe04eb9a
Eden Ridgway <eden.ridgway@gmail.com>
2013-09-02 21:17:57 +0100

    Now uses the options default sort order

--- slick.grid.js
+++ slick.grid.js
@@ -969,7 +969,7 @@ if (typeof Slick === "undefined") {
                         if (!sortOpts) {
                             sortOpts = {
                                 columnId: column.id,
-                                sortAsc: true
+                                sortAsc: column.defaultSortAsc
                             };
                             sortColumns.push(sortOpts);
                         } else if (sortColumns.length == 0) {
